93c924cd1819af1b743e7b52a76de71f8eb3d5c5
Ólafur Pálsson <olp10@hi.is>
2024-03-30 14:47:58 +0000

    Hæ

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/components/App';
 import {name as appName} from './app.json';
+
+<script src="http://localhost:8097"></script>;
 
 AppRegistry.registerComponent(appName, () => App);
