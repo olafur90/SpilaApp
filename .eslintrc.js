354f04000de60af057f87fa4762599174295cc9a
olafur90 <olafur1990@gmail.com>
2024-03-30 22:15:58 +0000

    Added empty Yatzy, Scrabble and Rommi components

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: '@react-native',
   rules: {
     'object-curly-spacing': ['error', 'always'],
+    'linebreak-style': 'off',
   },
 };
---
+++ src/components/games/Yatzy.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Yatzy() {
+  return <div>Yatzy</div>;
+}
