9f45cc112899f3e0a01fdc0e6f696d07224ef712
Ólafur Pálsson <olp10@hi.is>
2024-04-04 21:03:05 +0000

    Modal works

--- src/components/games/Yatzy/Yatzy.jsx
+++ src/components/games/Yatzy/Yatzy.jsx
@@ -7,7 +7,16 @@
  */
 
 import React, { useState } from 'react';
-import { Button, ImageBackground, Text, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  ImageBackground,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { PlayerSection } from './PlayerSection';
 import { YatzyStyles } from './YatzyStyles';
 
@@ -17,6 +26,7 @@ export default function Yatzy() {
     { playerName: 'Birna', score: 0, finishedAllMoves: false },
   ]);
   const [gameOver, setGameOver] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const gameBackgroundImageURI = {
     uri: 'https://play-lh.googleusercontent.com/AWFhjdqGOodPYMI8BtJssHkc93QCkVCnC5SZOr25YDp5e-4bNkNTKfOfXSwVtbfdsiVC',
@@ -86,7 +96,82 @@ export default function Yatzy() {
           </View>
         )}
       </ImageBackground>
-      <Button style={YatzyStyles.submitGame} title="Skrá niðurstöður" />
+
+      {/* Button for submitting and ending the game */}
+      <Button
+        style={YatzyStyles.submitGame}
+        title="Skrá niðurstöður"
+        disabled={!gameOver}
+        onPress={() => setModalVisible(true)}
+      />
+      {gameOver && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Game over</Text>
+              <Text style={styles.modalText}>
+                {`Sigurvegari er: ${players[0].playerName}`}
+              </Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      )}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
